a214990a24d2fe5c0bf7314d3b20e91869409099
kmobs <kmobs@kmobli.com>
2020-05-23 08:59:26 -0500

    Filament load/unload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -965,11 +965,11 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -38.50, -0.880 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -38.50, -0.960 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 15
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
