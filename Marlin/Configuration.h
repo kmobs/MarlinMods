db4287b62e5e4b3dba60db779b2777147a9ffeec
kmobs <kmobs@kmobli.com>
2020-06-21 21:05:25 -0500

    Update for LinAdv and Microswiss

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -488,9 +488,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 27.71
+  #define DEFAULT_Ki 2.64
+  #define DEFAULT_Kd 72.73
 
 #endif // PIDTEMP
 
