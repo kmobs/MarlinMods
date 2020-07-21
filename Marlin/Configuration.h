2a7dceef7e40c977507992adb956544319fd10ed
kmobs <kmobs@kmobli.com>
2020-07-21 16:41:23 -0500

    Changes to bedsize to account for probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1112,15 +1112,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 232
-#define Y_BED_SIZE 232
+#define X_BED_SIZE 228
+#define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS 248
 #define Z_MAX_POS 250
 
 /**
